17c9c675584a95cb2fa7a6fd7e61a2ccee878d75
asdasdadsasdsdasdasdasd <sunnckd@naver.com>
2024-04-23 18:11:32 +0900

    v1

---
+++ 01/01.js
@@ -0,0 +1,10 @@
+function handleClick(n) {
+    // document.querySelector('#msg').innerHTML = '<h1>안녕하세여</h1>';
+    document.querySelector('#msg').innerHTML ='<h1>안녕하세요!+$(n)</h1>';
+}
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.getElementById('msg').innerHTML = '안녕하세요';
+    document.querySelector('#msg').innerHTML = '<h1>안녕하세여</h1>';
+
+});
+
